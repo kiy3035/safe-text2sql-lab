02fe29f73d8003293b65ad1920da38538b217291
kiy3035 <kiy3035@test.123>
2026-08-30 10:27:25 +0900

    Set up the Spring Boot project foundation

---
+++ src/main/java/dev/safetext2sql/SafeText2SqlApplication.java
@@ -0,0 +1,12 @@
+package dev.safetext2sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeText2SqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeText2SqlApplication.class, args);
+    }
+}
